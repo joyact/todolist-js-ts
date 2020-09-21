3b06d85002e697d6ec62a888e384f33d1fca39f5
joyAct <joyleeact@gmail.com>
2020-09-21 22:07:09 +0900

    Added save button function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const range = document.getElementById('range');
 const fillBtn = document.querySelector('.fill-btn');
 const paintBtn = document.querySelector('.paint-btn');
 const currentColor = document.querySelector('.current-btn');
+const saveBtn = document.querySelector('.save-btn');
 const active = document.querySelector('.active');
 const INITIAL_COLOR = '#2c2c2c';
 
@@ -15,10 +16,11 @@ let filling = false;
 canvas.width = 1000;
 canvas.height = 550;
 
-// brush defalut style
+// canvas defalut style
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
-ctx.fillStyle = '';
 ctx.lineWidth = 5.0;
 
 // painting mode
@@ -65,6 +67,14 @@ const handleFillClick = () => {
   paintBtn.classList.remove('active');
 };
 
+const handelSaveClick = () => {
+  const image = canvas.toDataURL('image/png');
+  const link = document.createElement('a');
+  link.href = image;
+  link.download = 'Painter-JS';
+  link.click();
+};
+
 const handleRangeChange = (event) => {
   const targetSize = event.target.value;
   ctx.lineWidth = targetSize;
@@ -76,6 +86,7 @@ const handleColorClick = (event) => {
   ctx.fillStyle = targetColor;
   currentColor.style.color = targetColor;
 };
+
 // event listener
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
@@ -83,9 +94,11 @@ if (canvas) {
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
   canvas.addEventListener('click', handleCanvasClick);
+  // canvas.addEventListener('contextmenu', handleCM); // prevent clicking mouse right button
 }
 
 paintBtn.addEventListener('click', handlePaintClick);
 fillBtn.addEventListener('click', handleFillClick);
+saveBtn.addEventListener('click', handelSaveClick);
 range.addEventListener('input', handleRangeChange);
 colors.forEach((color) => color.addEventListener('click', handleColorClick));
