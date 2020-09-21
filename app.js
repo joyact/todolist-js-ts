887a325b15c78440f43f35c5166844de719db20c
joyAct <joyleeact@gmail.com>
2020-09-21 21:36:05 +0900

    Added paint and fill button function

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const colors = document.querySelectorAll('.color');
 const range = document.getElementById('range');
-
+const fillBtn = document.querySelector('.fill-btn');
+const paintBtn = document.querySelector('.paint-btn');
+const currentColor = document.querySelector('.current-btn');
+const active = document.querySelector('.active');
 const INITIAL_COLOR = '#2c2c2c';
+
 let painting = false;
+let filling = false;
 
 // pixel modifier : you have to give your canvas size here!
 canvas.width = 1000;
@@ -12,6 +17,8 @@ canvas.height = 550;
 
 // brush defalut style
 ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.fillStyle = '';
 ctx.lineWidth = 5.0;
 
 // painting mode
@@ -27,7 +34,7 @@ const startPainting = () => {
 const onMouseMove = (event) => {
   let x = event.offsetX;
   let y = event.offsetY;
-  if (!painting) {
+  if (!painting || filling) {
     // when moving mouse WITHOUT clicking
     // creating PATH
     ctx.beginPath();
@@ -40,26 +47,45 @@ const onMouseMove = (event) => {
   }
 };
 
+const handleCanvasClick = (event) => {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+};
+
+const handlePaintClick = () => {
+  filling = false;
+  paintBtn.classList.add('active');
+  fillBtn.classList.remove('active');
+};
+
+const handleFillClick = () => {
+  filling = true;
+  fillBtn.classList.add('active');
+  paintBtn.classList.remove('active');
+};
+
 const handleRangeChange = (event) => {
-  let targetSize = event.target.value;
+  const targetSize = event.target.value;
   ctx.lineWidth = targetSize;
 };
 
 const handleColorClick = (event) => {
-  let targetColor = event.target.style.backgroundColor;
+  const targetColor = event.target.style.backgroundColor;
   ctx.strokeStyle = targetColor;
+  ctx.fillStyle = targetColor;
+  currentColor.style.color = targetColor;
 };
-
-// mouse event
+// event listener
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
   canvas.addEventListener('mousedown', startPainting);
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
+  canvas.addEventListener('click', handleCanvasClick);
 }
 
-if (range) {
-  range.addEventListener('input', handleRangeChange);
-}
-
+paintBtn.addEventListener('click', handlePaintClick);
+fillBtn.addEventListener('click', handleFillClick);
+range.addEventListener('input', handleRangeChange);
 colors.forEach((color) => color.addEventListener('click', handleColorClick));
