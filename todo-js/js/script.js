2bb6684128803f5692d6711a3c349d7f1f6f6607
Joy Lee <joylee.dev@gmail.com>
2021-02-10 17:20:04 +0900

    Javascript Todolist - display task box when list is selected

--- todo-js/js/script.js
+++ todo-js/js/script.js
@@ -1,6 +1,9 @@
 const listContainer = document.querySelector('[data-lists]');
-const newListForm = document.querySelector('[data-new-list-form]');
-const newListInput = document.querySelector('[data-new-list-input]');
+const newListForm = document.querySelector('[data-list-form]');
+const newListInput = document.querySelector('[data-list-input]');
+const taskBox = document.querySelector('[data-task-box]');
+const taskTitle = document.querySelector('[data-task-title]');
+const taskCount = document.querySelector('[data-task-count]');
 
 const LOCAL_LIST_KEY = 'task.lists';
 const LOCAL_ACTIVE_LIST_KEY = 'task.activeList';
@@ -38,16 +41,36 @@ function createList(name) {
   };
 }
 
+function getTaskInfo(target) {
+  const incompleteTask = target.tasks.filter((task) => !task.complete).length;
+  const taskString = incompleteTask === 0 || 1 ? 'task' : 'tasks';
+  taskTitle.innerHTML = target.name;
+  taskCount.innerHTML = `${incompleteTask} ${taskString} remaining`;
+}
+
 // save information on localStorage
 function save() {
   localStorage.setItem(LOCAL_LIST_KEY, JSON.stringify(lists));
-  localStorage.setItem(LOCAL_ACTIVE_LIST_KEY, JSON.stringify(activeListId));
+  localStorage.setItem(LOCAL_ACTIVE_LIST_KEY, activeListId);
 }
 
 function render() {
   // remove current DOM elements
   clearElements(listContainer);
+  renderLists();
 
+  // render task container when the list is selected
+  const activeList = lists.find((list) => list.id === activeListId);
+
+  if (activeList == null) {
+    taskBox.style.display = 'none';
+  } else {
+    taskBox.style.display = 'block';
+    getTaskInfo(activeList);
+  }
+}
+
+function renderLists() {
   // create new li elements
   lists.forEach((list) => {
     const listElement = document.createElement('li');
