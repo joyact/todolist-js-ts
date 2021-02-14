7cb6bfc2e6844b41dbd0ea23b5b54a21c0da5265
Joy Lee <joylee.dev@gmail.com>
2021-02-14 22:12:23 +0900

    Create a html file and connect with a server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// serve files in the 'static' directory
+// path.resolve() returns a string with absolute path.
+app.use('/static', express.static(path.resolve(__dirname, 'src', 'static')));
+
+// request & response
+// every path which is sent to the web server, it will be sent to index.html
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'src', 'index.html'));
+});
+
+// listen for connections on the given port
+app.listen(process.env.POST || 3000, () => console.log('Server running...'));
