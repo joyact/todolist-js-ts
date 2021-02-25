32c7ade0b558aedb77ca66e73448a4582b09913c
Joy Lee <joylee.dev@gmail.com>
2021-02-25 18:46:39 +0900

    Add the Calculator & Clock App

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,7 +1,187 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Calculator() {
-  return <div>Calculator</div>;
+  useEffect(() => {
+    class Calculator {
+      constructor(prevOperandElement, currOperandElement) {
+        this.prevOperandElement = prevOperandElement;
+        this.currOperandElement = currOperandElement;
+        this.clear();
+      }
+
+      // initial display on load
+      clear() {
+        // declare 3 methods
+        this.prevOperand = '';
+        this.currOperand = '';
+        this.operation = undefined; // don't have any operation selected
+      }
+
+      // remove the last added number
+      delete() {
+        this.currOperand = this.currOperand.slice(0, -1);
+      }
+
+      // add the clicked number
+      appendNumber(number) {
+        //avoiding multiple dot('.')
+        if (number === '.' && this.currOperand.includes('.')) return;
+        this.currOperand += number;
+      }
+
+      // move numbers to the previous area
+      chooseOperation(operation) {
+        if (this.currOperand === '') return;
+        if (this.prevOperand !== '') {
+          this.compute();
+        }
+        this.operation = operation;
+        this.prevOperand = this.currOperand;
+        this.currOperand = '';
+      }
+
+      // compute the number
+      compute() {
+        let computation; //result of this function
+        const prev = parseFloat(this.prevOperand);
+        const curr = parseFloat(this.currOperand);
+        if (isNaN(prev) || isNaN(curr)) return;
+        switch (this.operation) {
+          case '+':
+            computation = prev + curr;
+            break;
+          case '-':
+            computation = prev - curr;
+            break;
+          case '*':
+            computation = prev * curr;
+            break;
+          case '÷':
+            computation = prev / curr;
+            break;
+          default:
+            return;
+        }
+        this.currOperand = computation;
+        this.operation = undefined;
+        this.prevOperand = '';
+      }
+
+      // handle the decimal place
+      getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]); // before the period(.)
+        const decimalDigits = stringNumber.split('.')[1]; // after the period(.)
+        let integerDisplay;
+
+        // add thousand delimiter (,)
+        if (isNaN(integerDigits)) {
+          integerDisplay = '';
+        } else {
+          integerDisplay = integerDigits.toLocaleString('en', {
+            maximumFractionDigits: 0, // zero decimal place
+          });
+        }
+
+        // add decimal point (.)
+        if (decimalDigits != null) {
+          return `${integerDisplay}.${decimalDigits}`;
+        } else {
+          return integerDisplay;
+        }
+      }
+
+      // make the clicked value printed
+      updateDisplay() {
+        this.currOperandElement.innerText = this.getDisplayNumber(
+          this.currOperand
+        );
+        if (this.operation != null) {
+          this.prevOperandElement.innerText = `${this.getDisplayNumber(
+            this.prevOperand
+          )} ${this.operation}`;
+        } else {
+          this.prevOperandElement.innerText = '';
+        }
+      }
+    }
+
+    // select elements
+    const numberBtn = document.querySelectorAll('[data-number]');
+    const operationBtn = document.querySelectorAll('[data-operation]');
+    const equalsBtn = document.querySelector('[data-equals]');
+    const deleteBtn = document.querySelector('[data-delete]');
+    const acBtn = document.querySelector('[data-ac]');
+    const prevOperandElement = document.querySelector('.previous-operand');
+    const currOperandElement = document.querySelector('.current-operand');
+
+    const calculator = new Calculator(prevOperandElement, currOperandElement);
+
+    //when number buttons are clicked
+    numberBtn.forEach((button) => {
+      button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+      });
+    });
+
+    //when operation buttons are clicked
+    operationBtn.forEach((button) => {
+      button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+      });
+    });
+
+    //when '=' is clicked
+    equalsBtn.addEventListener('click', () => {
+      calculator.compute();
+      calculator.updateDisplay();
+    });
+
+    //when 'AC' is clicked
+    acBtn.addEventListener('click', () => {
+      calculator.clear();
+      calculator.updateDisplay();
+    });
+
+    //when 'DEL' is clicked
+    deleteBtn.addEventListener('click', () => {
+      calculator.delete();
+      calculator.updateDisplay();
+    });
+  }, []);
+
+  return (
+    <div className="calculator">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <button data-ac className="span-two">
+        AC
+      </button>
+      <button data-delete>DEL</button>
+      <button data-operation>÷</button>
+      <button data-number>1</button>
+      <button data-number>2</button>
+      <button data-number>3</button>
+      <button data-operation>*</button>
+      <button data-number>4</button>
+      <button data-number>5</button>
+      <button data-number>6</button>
+      <button data-operation>+</button>
+      <button data-number>7</button>
+      <button data-number>8</button>
+      <button data-number>9</button>
+      <button data-operation>-</button>
+      <button data-number>.</button>
+      <button data-number>0</button>
+      <button data-equals className="span-two">
+        =
+      </button>
+    </div>
+  );
 }
 
 export default Calculator;
--- src/pages/Clock.js
+++ src/pages/Clock.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Clock() {
-  return <div>Clock</div>;
+  useEffect(() => {
+    const hourHand = document.querySelector('[data-hour-hand]');
+    const minuteHand = document.querySelector('[data-minute-hand]');
+    const secondHand = document.querySelector('[data-second-hand]');
+
+    const setClock = () => {
+      const currentDate = new Date();
+      const secondsRatio = currentDate.getSeconds() / 60;
+      const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+      const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+      //adding ratio to the value makes hands gradually move
+      setRotation(secondHand, secondsRatio);
+      setRotation(minuteHand, minutesRatio);
+      setRotation(hourHand, hoursRatio);
+    };
+
+    const setRotation = (element, rotationRatio) => {
+      element.style.setProperty('--rotation', rotationRatio * 360);
+    };
+
+    setInterval(setClock, 1000);
+    setClock();
+  }, []);
+
+  return (
+    <div className="clock">
+      <div className="hand hour" data-hour-hand></div>
+      <div className="hand minute" data-minute-hand></div>
+      <div className="hand second" data-second-hand></div>
+      <div className="number number1">1</div>
+      <div className="number number2">2</div>
+      <div className="number number3">3</div>
+      <div className="number number4">4</div>
+      <div className="number number5">5</div>
+      <div className="number number6">6</div>
+      <div className="number number7">7</div>
+      <div className="number number8">8</div>
+      <div className="number number9">9</div>
+      <div className="number number10">10</div>
+      <div className="number number11">11</div>
+      <div className="number number12">12</div>
+    </div>
+  );
 }
 
 export default Clock;
