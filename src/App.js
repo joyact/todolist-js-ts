707b055f2fa20d3d2192392934192997c27b5185
Joy Lee <joylee.dev@gmail.com>
2021-03-01 19:24:02 +0900

    media query & navbar on mobile

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './components/Nav';
 import { navs } from './data/navData';
 
-const App = () => (
-  <Router>
-    <Nav />
-    <Switch>
-      <>
-        <div id="content">
-          {navs.map((navItem) => (
-            <Route
-              exact
-              key={navItem.id}
-              path={navItem.path}
-              component={navItem.component}
-            />
-          ))}
-        </div>
-      </>
-    </Switch>
-  </Router>
-);
+const App = () => {
+  const [navOpen, setNavOpen] = useState(false);
+  const handleToggle = () => {
+    setNavOpen(!navOpen);
+  };
+
+  return (
+    <Router>
+      <Nav open={navOpen} onClick={handleToggle} />
+      <Switch>
+        <>
+          <div id="content">
+            <i className="toggle-btn" onClick={handleToggle}></i>
+            {navs.map((navItem) => (
+              <Route
+                exact
+                key={navItem.id}
+                path={navItem.path}
+                component={navItem.component}
+              />
+            ))}
+          </div>
+        </>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { navs } from '../data/navData';
 
-const Nav = () => (
-  <nav>
+const Nav = ({ open, onClick }) => (
+  <nav className={open ? 'active' : ''}>
     <h1 className="nav-logo">wediz</h1>
     <ul>
       {navs.map((navItem) => (
-        <li className="nav-link" key={navItem.id}>
+        <li className="nav-link" key={navItem.id} onClick={onClick}>
           <NavLink exact to={navItem.path} activeClassName="nav-active">
             {navItem.name}
           </NavLink>
