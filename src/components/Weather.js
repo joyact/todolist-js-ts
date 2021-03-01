f54a2eec244c8e89abd9ffb235c30f7d566ecde3
Joy Lee <joylee.dev@gmail.com>
2021-03-01 18:13:47 +0900

    Add icons from ReactAnimatedWeather

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import ReactAnimatedWeather from 'react-animated-weather';
 
 function Weather() {
   const [weather, setWeather] = useState({
     city: undefined,
-    country: undefined,
     temp_current: undefined,
     temp_max: undefined,
     temp_min: undefined,
+    current: undefined,
+    icon: undefined,
   });
 
   const convertCelsius = (temp) => {
@@ -14,18 +16,51 @@ function Weather() {
     return cel;
   };
 
+  // icon canvas style
+  const defaults = {
+    color: 'white',
+    size: 100,
+    animate: true,
+  };
+
+  // icon
+  const weatherIcon = (current) => {
+    switch (current) {
+      case 'Haze':
+        return 'CLEAR_DAY';
+      case 'Clouds':
+        return 'CLOUDY';
+      case 'Snow':
+        return 'SNOW';
+      case 'Drizzle':
+        return 'SLEET';
+      case 'Dust':
+      case 'Tornado':
+        return 'WIND';
+      case 'Fog':
+      case 'Smoke':
+        return 'FOG';
+      case 'Rain':
+      case 'Mist':
+        return 'RAIN';
+      default:
+        return 'CLEAR_DAY';
+    }
+  };
+
   const getWeather = async (lat, lon) => {
     const API_KEY = '9916e4e6fd6079aa9a9fec8e0c218fc5';
-    const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=kr`;
     const response = await fetch(API_URL);
     const data = await response.json();
 
     setWeather({
       city: data.name,
-      country: data.country,
       temp_current: convertCelsius(data.main.temp),
       temp_max: convertCelsius(data.main.temp_max),
       temp_min: convertCelsius(data.main.temp_min),
+      current: data.weather[0].main,
+      icon: weatherIcon(data.weather[0].main),
     });
   };
 
@@ -50,26 +85,28 @@ function Weather() {
   }, []);
 
   return (
-    <div>
-      <h1>
-        {weather.city}, {weather.country}
-      </h1>
-      <h2>{weather.temp_current}&deg;</h2>
-      <h3>
-        <span>{weather.temp_min}&deg;</span>
-        <span>{weather.temp_max}&deg;</span>
-      </h3>
-      <h3>Slow Rain</h3>
+    <div className="weather">
+      <div className="weather-info">
+        <h1 className="location">{weather.city}</h1>
+        <div className="temperature">
+          <p className="current">{weather.temp_current}&deg;</p>
+          <ul className="minmax">
+            <li>{weather.temp_min}&deg;</li>
+            <li>{weather.temp_max}&deg;</li>
+          </ul>
+        </div>
+      </div>
+      <div className="weather-icon">
+        <ReactAnimatedWeather
+          icon={weather.icon ? weather.icon : 'CLEAR_DAY'}
+          color={defaults.color}
+          size={defaults.size}
+          animate={defaults.animate}
+        />
+        <p className="state">{weather.current}</p>
+      </div>
     </div>
   );
 }
 
-function minmaxTemp(min, max) {
-  return (
-    <h3>
-      <span>{min}&deg;</span>
-      <span>{max}&deg;</span>
-    </h3>
-  );
-}
 export default Weather;
